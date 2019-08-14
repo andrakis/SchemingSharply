5085e8e77a8aede38ceffc95f1feeb29ab9abc27
Daedalus <daedalus.raistlin@gmail.com>
2019-08-14 12:49:55 +1000

    Add GCC Makefile
    Generated from Netbeans

--- alisp/nbproject/Makefile-variables.mk
+++ alisp/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_x86_64-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_x86_64-Windows
-CND_ARTIFACT_NAME_Debug=schemingplusplus
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/schemingplusplus
+CND_ARTIFACT_NAME_Debug=alisp
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/alisp
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_x86_64-Windows/package
-CND_PACKAGE_NAME_Debug=schemingplusplus.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/package/schemingplusplus.tar
+CND_PACKAGE_NAME_Debug=alisp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_x86_64-Windows/package/alisp.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_x86_64-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_x86_64-Windows
-CND_ARTIFACT_NAME_Release=schemingplusplus
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_x86_64-Windows/schemingplusplus
+CND_ARTIFACT_NAME_Release=alisp
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_x86_64-Windows/alisp
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_x86_64-Windows/package
-CND_PACKAGE_NAME_Release=schemingplusplus.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_x86_64-Windows/package/schemingplusplus.tar
+CND_PACKAGE_NAME_Release=alisp.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_x86_64-Windows/package/alisp.tar
 #
 # include compiler specific variables
 #
--- alisp/nbproject/Package-Debug.bash
+++ alisp/nbproject/Package-Debug.bash
@@ -7,15 +7,15 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=Cygwin_x86_64-Windows
-CND_CONF=Release
+CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/schemingplusplus
-OUTPUT_BASENAME=schemingplusplus
-PACKAGE_TOP_DIR=schemingplusplus/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/alisp
+OUTPUT_BASENAME=alisp
+PACKAGE_TOP_DIR=alisp/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/schemingplusplus/bin"
+makeDirectory "${NBTMPDIR}/alisp/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/schemingplusplus.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/alisp.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/schemingplusplus.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/alisp.tar *
 checkReturnCode
 
 # Cleanup
